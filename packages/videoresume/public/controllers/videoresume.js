b9b80b0136963722a4877fdfc7a490eb40ceed65
Nikolay Vinogradskiy <nivino@STI-WR-008.(none)>
2014-06-17 16:46:30 +0200

    controller restructure

--- packages/videoresume/public/controllers/videoresume.js
+++ packages/videoresume/public/controllers/videoresume.js
@@ -2,98 +2,51 @@
 
 angular.module('mean')
 
-// service('OpentokData', function($http, $q) {
-//     this.get = function(){
-//       var deferred = $q.defer();
-//       $http.get('/videoresume/host')
-//         .success(function(data, status) {
-//           deferred.resolve(data);
-//         })
-//         .error(function(data, status) {
-//           deferred.reject(data);
-//         });
-
-//       return deferred.promise;
-//     } 
-//   }
-// );
-
-.controller('VideoresumeController', ['$scope', '$http', 'Global', 'OTSession', 'TB', '$log',
-  function($scope, $http, Global, OTSession, TB, $log, Videoresume) {
+.controller('VideoresumeController', ['$scope', '$rootScope', '$http', 'Global', 'OTSession', 'TB', '$log',
+  function($scope, $rootScope, $http, Global, OTSession, TB, $log, Videoresume) {
     $scope.global = Global;
 
-    $scope.startArchive = function() {
-      $http.get('/videoresume/host').then(function(data) {
+    $rootScope.$on("$stateChangeSuccess", function (event, current, previous, rejection) {
+      console.log(current);
+      if(current == '/videoresume/host') {
+        var archiveID = null;
+        $http.get('/videoresume/host').then(function(data) {
+          var session = TB.initSession(data.sessionId),
+              publisher = TB.initPublisher('publisher');
 
-        // return console.log(data);
-
-        var session = TB.initSession(data.sessionId),
-            publisher = TB.initPublisher('publisher');
-                // archiveID = null;
-
-
-        session.connect(data.apiKey, data.token, function(err, info) {
-          if(err) {
-            alert(err.message || err);
-          }
-          session.publish(publisher);
-        });
-
-        session.on('streamCreated', function(event) {
-          session.subscribe(event.stream, 'subscribers', { insertMode: 'append' });
-        });
-      });
-    };
-
-    $scope.stopArchive = function() {
-    	$http.get('videoresume/stop')
-    		.success(function(data) {
-
-          var apiKey = data.apiKey,
-              sessionId = data.sessionId,
-              token = data.token;
-
-          var session = OTSession.initSession(sessionId),
-              publisher = OTSession.initPublisher('publisher'),
-              archiveID = null;
-
-          session.connect(apiKey, token, function(err, info) {
+          session.connect(data.apiKey, data.token, function(err, info) {
             if(err) {
               alert(err.message || err);
             }
             session.publish(publisher);
           });
+        });
+      }
+    });
 
-          session.on('streamCreated', function(event) {
-            session.subscribe(event.stream, 'subscribers', { insertMode: 'append' });
-          });
 
-          session.on('archiveStarted', function(event) {
-            archiveID = event.id;
-            console.log('ARCHIVE STARTED');
-            // $('.start').hide();
-            // $('.stop').show();
-          });
+    $scope.$on('archiveStarted', function(e) {
+      archiveId = event.id;
+      $log.info('Recording started');
+      $scope.disableToggle = true; // disable button "start"
+    });
 
-          session.on('archiveStopped', function(event) {
-            archiveID = null;
-            console.log('ARCHIVE STOPPED');
-            // $('.start').show();
-            // $('.stop').hide();
-          });
+    $scope.$on('archiveStopped', function(e) {
+      archiveId = null;
+      $log.info('Recording stopped');
+      $scope.disableToggle = false;
+    });
 
-          // $(document).ready(function() {
-          //   $('.start').click(function(event){
-          //     $.get('start');
-          //   }).show();
-          //   $('.stop').click(function(event){
-          //     $.get('stop/' + archiveID);
-          //   }).hide();
-          // });
-    		})
-    		.error(function(err) {
-    			return console.log(err);
-    		});
+    $scope.startRecording = function() {
+      $http.get('/videoresume/start').then(function() {
+        $scope.disableToggle = true; // ????????????
+      });
     };
-  }
+
+    $scope.stopRecording = function() {
+      $http.get('/videoresume/stop' + archiveId).then(function() {
+        $scope.disableToggle = true; // ????????????
+      });
+    };
+  };
 ]);
